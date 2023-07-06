bfb1e3078009eddbaadae6fe57ae97e8aa09cc72
Zara Syversen <zara.syversen@ninetech.com>
2023-07-06 15:50:00 +0200

    add empty list cmp

---
+++ src/lib/todos/emptyList.svelte
@@ -0,0 +1,13 @@
+<div>
+    <div class="empty-list">
+      <h2>Wow, nothing to do yet?</h2>
+      <p>Get started by adding a new task to your list!</p>
+    </div>
+</div>
+  
+<style>
+.empty-list {
+    margin-top: 30px;
+    text-align: center;
+}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,20 @@
 <script lang="ts">
     import Header from "../lib/header.svelte";
     import Counter from "../lib/todos/counter.svelte";
+	import EmptyList from "../lib/todos/emptyList.svelte";
 	import type { TodoItem } from "../types/TodoItem";
 
-    const array: TodoItem[] = [{name: 'hej', id:1, completed: false}, {name: 'hej2', id:3, completed: true}]
+    //const todos: TodoItem[] = [{name: 'hej', id:1, completed: false}, {name: 'hej2', id:3, completed: true}];
+    const todos: TodoItem[] = [];
 </script>
 <div class="container">
     <div class="inner">
       <div class="inner-top">
         <Header />
-        <Counter todos={array} />
+        <Counter todos={todos} />
+        {#if todos.length === 0}
+            <EmptyList />
+        {/if}
       </div>
     </div>
 </div>
