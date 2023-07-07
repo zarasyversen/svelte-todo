dcddc0a7ffa55d4383b536c717515d0ad3375ec7
Zara Syversen <zara.syversen@ninetech.com>
2023-07-07 16:00:29 +0200

    Add test for add and check todo

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,20 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h1', async ({ page }) => {
   await page.goto('/');
-  await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible();
+  await expect(page.getByRole('heading', { name: 'Your list of tasks' })).toBeVisible();
+});
+
+test('add todo', async({ page}) => {
+  await page.goto('/');
+  await page.getByLabel('Add a new task').fill('Add new todo');
+  await page.getByRole('button').click();
+  await expect(page.getByText('Add new todo')).toBeVisible();
+});
+
+test('complete todo', async({ page}) => {
+  await page.goto('/');
+  await page.getByLabel('Add a new task').fill('Add new todo');
+  await page.getByRole('button').click();
+  await page.locator('.checkboxLabel').click();
+  await expect(page.locator('.details')).toHaveCSS('font-style', 'italic');
 });
